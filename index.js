77375db4fd846b3a555cf0a9f6e04d903a895bf1
+Sam Thongo <+samuelthongo73@gmail.com>
2023-06-15 16:57:34 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickLocation){
+ const headquarters = 42
+ const distanceFromHqInBlocks = Math.abs(headquarters - pickLocation)
+ return distanceFromHqInBlocks
+ }
+
+function distanceFromHqInFeet(pickLocation){
+    const sizeofblock = 264
+    const headquarters = 42
+    const distanceFromHqInBlocks = Math.abs(headquarters - pickLocation)
+    const distanceFromHqInFeet = distanceFromHqInBlocks * sizeofblock
+    return distanceFromHqInFeet
+}
+function distanceTravelledInFeet(start, destination){
+    const distanceTravelledInFeet = Math.abs(start - destination) * 264
+    return distanceTravelledInFeet
+}
+function calculatesFarePrice(start, destination){
+    const distanceTravelledInFeet = Math.abs(start - destination) * 264
+    if (distanceTravelledInFeet <= 400){
+        return 0
+    }else if(distanceTravelledInFeet > 400 && distanceTravelledInFeet<= 2000){
+        const fareprice = 0.02 * (distanceTravelledInFeet - 400)
+        return fareprice
+    }else if(distanceTravelledInFeet > 2000 && distanceTravelledInFeet <= 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+}
